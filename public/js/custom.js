7cccf989539b88d8742fe4aa1f9b6b7d0e9e5ad4
Wan Muzaffar <wanmuz86@gmail.com>
2017-05-03 13:29:55 +0800

    backup

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,19 @@
 $(function() {
-	var i = $('assignment p').size() + 1;
-$('.addassignment').click(function(){
-	  $('<p><label for="p_questions"><input type="text" id="p_scnt" size="20" name="p_scnt_' + i +'" value="" placeholder="Input Value" /></label> <a href="#" id="remScnt">Remove</a></p>').appendTo(assignment);
-                i++;
-                return false;
-});
+	var i = $('.add-answer p').size() + 1;
+	$('.add-answer').click(function(){
+
+		$('<p><label for="p_answers"><input type="text" id="p_scnt" size="30" name="answers[]" value="" placeholder="Input Answer" /></label> <a href="#" class="remScnt">Remove</a></p>').appendTo($('#div_opt'));
+		i++;
+		return false;
+	});
+
+
+	$('.remScnt').click(function() { 
+		alert('pressed');
+		if( i > 2 ) {
+			$(this).parents('p').remove();
+			i--;
+		}
+		return false;
+	});
 });
